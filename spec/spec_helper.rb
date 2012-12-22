60a2c5e58d282f4bdb901232bd9ba32f14c11f81
Paul Hepworth <paul.hepworth@peppyheppy.com>
2012-12-22 15:41:02 -0800

    added default site id to config options; still defaults to EBAY_US though

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
-require 'rspec'
+require 'bundler/setup'
+require File.join(File.dirname(__FILE__), "..", "lib", "rebay")
+
+Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
+
 RSpec.configure do |config|
   config.mock_with :rspec
 end
 
-require File.join(File.dirname(__FILE__), "..", "lib", "rebay")
-
 Rebay::Api.configure do |rebay|
-  rebay.app_id = 'test'
+  rebay.app_id = 'default'
 end
